ef1702d97e9232fd38c186561ae84003f10cafff
Mark Arzangulyan <15670678+Arzangulyan@users.noreply.github.com>
2024-01-15 15:26:16 +0300

    regular gas&gss after december

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,12 +1,12 @@
 export const Stipend = {
 	gas: {
-		only4: 5200,
-		miss: 5200,
-		with4and5: 5980,
-		only5: 6500,
+		only4: 2941,
+		miss: 2941,
+		with4and5: 3382,
+		only5: 3677,
 	},
 	pgas: 14261,
-	gss: 7800,
+	gss: 4413,
 	pgss: 14269,
 };
 
