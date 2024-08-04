6f1581f751e03e3e662c9ba2e33d3605e8cc20f8
Mark Arzangulyan <15670678+Arzangulyan@users.noreply.github.com>
2024-08-04 13:11:17 +0300

    update Fall sums for GAS/GSS

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,12 +1,12 @@
 export const Stipend = {
 	gas: {
-		only4: 2941,
-		miss: 2941,
-		with4and5: 3382,
-		only5: 3677,
+		only4: 3091,
+		miss: 3091,
+		with4and5: 3555,
+		only5: 3865,
 	},
 	pgas: 13685,
-	gss: 4413,
+	gss: 4638,
 	pgss: 14364,
 };
 
