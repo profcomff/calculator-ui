b1ce7c71d88099e618b3245b9987af1b61d6173a
default <batuev.io18@physics.msu.ru>
2024-09-30 10:17:03 +0000

    fixed tabs and design

--- src/vuetify.ts
+++ src/vuetify.ts
@@ -1,6 +1,26 @@
 import { createVuetify } from 'vuetify';
 import { aliases, mdi } from 'vuetify/iconsets/mdi-svg';
 
+const profcomffLight = {
+	dark: false,
+	colors: {
+		primary: 'rgb(0, 1, 76)',
+		'on-primary': 'rgb(255, 255, 255)',
+
+		secondary: 'rgb(255, 139, 0)',
+		'on-secondary': 'rgb(255, 255, 255)',
+
+		background: 'rgb(255, 255, 255)',
+		'on-background': 'rgb(0, 0, 0)',
+
+		surface: 'rgb(255, 255, 255)',
+		'on-surface': 'rgb(0, 0, 0, 0.87)',
+
+		'surface-variant': 'rgb(255, 255, 255)',
+		'on-surface-variant': 'rgba(0, 0, 0, 0.87)',
+	},
+};
+
 export const vuetify = createVuetify({
 	icons: {
 		defaultSet: 'mdi',
@@ -16,6 +36,12 @@ export const vuetify = createVuetify({
 		},
 		VCheckbox: {
 			color: 'rgb(0, 1, 76)',
+		},
+	},
+	theme: {
+		defaultTheme: 'profcomffLight',
+		themes: {
+			profcomffLight,
 		},
 	},
 });
